b975e15a0a4ec97f7929a7ca8e0f0628b8540a7e
Arjun Gupta <saimzn4241@gmail.com>
2017-07-15 16:39:12 +0530

    home and news

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { HttpModule } from '@angular/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { NewsPage } from '../pages/news/news';
+import { DetailsPage } from '../pages/details/details';
 import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -13,17 +16,22 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    NewsPage,
+    DetailsPage,
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    NewsPage,
+    DetailsPage,
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {NewsPage} from '../news/news';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
   constructor(public navCtrl: NavController) {
-
   }
-
+  news(){
+    this.navCtrl.push(NewsPage, {
+    })
+  }
 }
