8002d58893554355f086d53bfc29bb5bc43a835c
Arjun Gupta <saimzn4241@gmail.com>
2017-07-22 13:12:53 +0530

    login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { SignupPage } from '../signup/signup';
-import {Http, URLSearchParams} from '@angular/http';
+import {Http, URLSearchParams, RequestOptions, Headers} from '@angular/http';
 import 'rxjs/Rx';
+import {HomePage} from '../home/home';
+
 
 @Component({
   selector: 'page-login',
@@ -13,25 +15,43 @@ export class LoginPage {
   baseUrl: string;
   username: string;
   password: string;
-
+  errorMsg: string;
   constructor(public navCtrl: NavController, private http: Http) {
     this.baseUrl = '/login';
+    this.errorMsg = null;
   }
   gotoSignup() {
     this.navCtrl.push(SignupPage, {
     })
   }
 
+  getCookie(name) {
+    let value = "; " + document.cookie;
+    let parts = value.split("; " + name + "=");
+    if (parts.length == 2)
+      return parts.pop().split(";").shift();
+  }
+
   login() {
-
-    let data = new URLSearchParams();
-    data.append('username', this.username);
-    data.append('password', this.password);
-
-    return this.http.post(this.baseUrl, data)
+    let headers = new Headers({
+      'Content-Type': 'application/json',
+      'X-CSRFToken': this.getCookie('csrftoken')
+    });
+    let options = new RequestOptions({ headers: headers });
+    
+    return this.http.post(this.baseUrl, { username: this.username, password: this.password }, options)
       .map(res => res.json())
       .subscribe(data => {
         console.log("login data=>", data);
+        if (data["error"] == false) {
+          this.errorMsg = null;
+          this.navCtrl.push(HomePage, {
+            'username': this.username,
+          })
+        }
+        else {
+          this.errorMsg = "Invalid username or password!!!";
+        }
       }, error => {
         console.log(error);
       });
